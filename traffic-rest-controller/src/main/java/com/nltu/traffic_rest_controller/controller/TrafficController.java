51b287ab3414eb888c7d095c59209040914c4bae
Yurii Midianyi <loopfordev@gmail.com>
2025-08-30 18:58:40 +0300

    stable

--- traffic-rest-controller/src/main/java/com/nltu/traffic_rest_controller/controller/TrafficController.java
+++ traffic-rest-controller/src/main/java/com/nltu/traffic_rest_controller/controller/TrafficController.java
@@ -1,7 +1,10 @@
 package com.nltu.traffic_rest_controller.controller;
 
 import com.nltu.traffic_rest_controller.entity.TrafficData;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/traffic")
@@ -9,21 +12,28 @@ public class TrafficController {
 
     private TrafficData trafficData = new TrafficData();
 
-    public TrafficController() {
-        trafficData.setRed(5);
-        trafficData.setGreen(15);
-        trafficData.setYellow(2);
-    }
+    @Value("${esp32.url:http://localhost:8180}")
+    private String esp32Url;
+
+    private final RestTemplate restTemplate = new RestTemplate();
 
     @PostMapping("/update")
     public String updateTraffic(@RequestBody TrafficData data) {
         this.trafficData = data;
-        System.out.println("Received from ESP32: " + data.getRed() + ", " + data.getGreen() + ", " + data.getYellow());
-        return "{\"status\": \"success\"}";
-    }
+        //System.out.println("Received from client: " + data.getRed() + ", " + data.getGreen() + ", " + data.getYellow());
+        String url = esp32Url + "/setTrafficData"
+                + "?q1=" + data.getQ1()
+                + "&s1=" + data.getS1()
+                + "&q2=" + data.getQ2()
+                + "&s2=" + data.getS2();
 
-    @GetMapping("/intervals")
-    public TrafficData getIntervals() {
-        return trafficData;
+        try {
+            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+            System.out.println("ESP32 responded: " + response.getBody());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "{\"status\": \"error\", \"message\": \"" + e.getMessage() + "\"}";
+        }
+        return "{\"status\": \"success\"}";
     }
 }
--- traffic-rest-controller/src/main/java/com/nltu/traffic_rest_controller/entity/TrafficData.java
+++ traffic-rest-controller/src/main/java/com/nltu/traffic_rest_controller/entity/TrafficData.java
@@ -2,16 +2,34 @@ package com.nltu.traffic_rest_controller.entity;
 
 
 public class TrafficData {
-    private int red;
-    private int green;
-    private int yellow;
 
-    public int getRed() { return red; }
-    public void setRed(int red) { this.red = red; }
+    private float q1;
+    private float q2;
+    private float s1;
+    private float s2;
 
-    public int getGreen() { return green; }
-    public void setGreen(int green) { this.green = green; }
+    public TrafficData() {}
 
-    public int getYellow() { return yellow; }
-    public void setYellow(int yellow) { this.yellow = yellow; }
+    public TrafficData(float s2, float s1, float q2, float q1) {
+        this.s2 = s2;
+        this.s1 = s1;
+        this.q2 = q2;
+        this.q1 = q1;
+    }
+
+    public float getQ1() {return q1;}
+
+    public void setQ1(float q1) {this.q1 = q1;}
+
+    public float getQ2() {return q2;}
+
+    public void setQ2(float q2) {this.q2 = q2;}
+
+    public float getS1() {return s1;}
+
+    public void setS1(float s1) {this.s1 = s1;}
+
+    public float getS2() {return s2;}
+
+    public void setS2(float s2) {this.s2 = s2;}
 }
